7f43fbe350fafb2e7dfdff04407103924da51c52
Thirsha <1991thirsha@gmail.com>
2021-07-26 21:03:52 -0700

    Deleted Deal button

--- SampleGitProject/ContentView.swift
+++ SampleGitProject/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+                    Image("background").ignoresSafeArea()
+                    VStack{
+                        
+                        Spacer()
+                        Image("logo")
+                        
+                        HStack{
+                            Spacer()
+                            Image("card2")
+                            Spacer()
+                            Image("card3")
+                            Spacer()
+                        }
+                        Spacer()
+              
+                        Spacer()
+                        HStack{
+                            Spacer()
+                            VStack{
+                                Text("Player").font(.headline).foregroundColor(Color.white).padding(.bottom,1.0)
+                                Text("0")
+                                    .font(.largeTitle)
+                                    .foregroundColor(Color.white)
+                            }
+                            Spacer()
+                            VStack{
+                                Text("CPU").font(.headline).foregroundColor(Color.white)
+                                    .padding(.bottom,1.0)
+                                Text("0")
+                                    .font(.largeTitle)
+                                    .foregroundColor(Color.white)
+                            }
+                            Spacer()
+                        }
+                        Spacer()
+                    
+                    }
+                }
     }
 }
 
