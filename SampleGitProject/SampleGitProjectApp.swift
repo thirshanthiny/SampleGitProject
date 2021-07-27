7262d8c501424614c11602e17852cda184128657
Thirsha <1991thirsha@gmail.com>
2021-07-26 20:28:57 -0700

    Initial Commit

---
+++ SampleGitProject/SampleGitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGitProjectApp.swift
+//  SampleGitProject
+//
+//  Created by Mac Os on 2021-07-26.
+//
+
+import SwiftUI
+
+@main
+struct SampleGitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
